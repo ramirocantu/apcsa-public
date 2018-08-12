ad559a9da09a76ef9de0dc445fb931a45f4e6aeb
Steve Hollasch <--show-origin>
2018-08-11 23:21:11 -0700

    2deep: md-to-markdeep.py converts link syntax
    This script now properly reads UTF-8 from stdin and writes UTF-8 to
    stdout.
    
    It gathers deferred-definition links, and then converts all occurrences
    to use the Markdeep style, where trailing empty square brackets are
    used.
    
    Markdeep interprets text inside square brackets as regular text, as long
    as it's not followed by empty square brackets (see above). This version
    of the converter finds all escaped square bracket text and removes the
    backslashes. For example, "Goodbye \[cruel\] world" is converted to
    "Goodbye [cruel] world".

--- lab/markdeep-curriculum/tools/md-to-markdeep.py
+++ lab/markdeep-curriculum/tools/md-to-markdeep.py
@@ -8,14 +8,17 @@ import sys
 def main():
     # Main Program Entry Point
 
-    # Set output stream to use UTF-8 encoding.
-    if sys.stdout.encoding != 'utf-8':
-        sys.stdout = codecs.getwriter('utf-8')(sys.stdout.buffer, 'strict')
+    setStdioUTF8()
+
+    lines = sys.stdin.readlines()
+
+    # Collect all deferred link definitions (usually at the bottom of the page).
+    linkPatterns = getLinkDefinitions(lines)
 
     printHeader()
 
-    for line in sys.stdin:
-        processLine (line.rstrip())
+    for line in lines:
+        processLine(line, linkPatterns)
 
     printFooter()
 
@@ -23,14 +26,23 @@ def main():
 
 
 #-----------------------------------------------------------------------------------------------------------------------
+def setStdioUTF8():
+    # Set input & output streams to use UTF-8 encoding.
+    if sys.stdout.encoding != 'utf-8':
+        sys.stdout = codecs.getwriter('utf-8')(sys.stdout.buffer, 'strict')
+
+    if sys.stdin.encoding != 'utf-8':
+        sys.stdin = codecs.getreader('utf-8')(sys.stdin.buffer, 'strict')
+
+
+#-----------------------------------------------------------------------------------------------------------------------
 def printHeader():
-    print ("<meta charset='utf-8'>")
-    print ("<!-- " + ("=" * 91) + " -->\n")
+    print ("<meta charset='utf-8'>\n\n")
 
 
 #-----------------------------------------------------------------------------------------------------------------------
 def printFooter():
-    print ("\n\n<!-- Markdeep: -->")
+    print ("\n\n<!-- " + (91 * "~") + " -->")
     print ("<style class='fallback'>body{visibility:hidden;white-space:pre;font-family:monospace}</style>")
     print ("<script src='markdeep.min.js'></script>")
     print ("<script src='https://casual-effects.com/markdeep/latest/markdeep.min.js'></script>")
@@ -38,7 +50,56 @@ def printFooter():
 
 
 #-----------------------------------------------------------------------------------------------------------------------
-def processLine(line):
+
+def getLinkDefinitions(lines):
+    # Scan through all lines and find link definitions (usually at the bottom of the page). Store these as regular
+    # expressions to match later.
+
+    linkDefPattern = re.compile(r'\[[^\]]+\]: ')
+    linkPatterns = []
+
+    for line in lines:
+        matchedLinkDef = linkDefPattern.match(line)
+        if matchedLinkDef != None:
+            linkRegExp = r'[^\]]\[' + line[1:matchedLinkDef.end()-3] + r'\][^:\[]'
+            linkPatterns.append (re.compile(linkRegExp))
+
+    return linkPatterns
+
+
+#-----------------------------------------------------------------------------------------------------------------------
+
+literalBracketPattern = re.compile(r'\\\[[^\]]+\\\]')
+
+def processLine (line, linkPatterns):
+    line = line.rstrip()
+
+    # Convert deferred link definitions to use empty trailing square brackets. For example, "see the [foo] page"
+    # (GitHub Markdown) would be converted to "see the [foo][] page" (Markdeep).
+
+    # The link regular expression ensures that some leading and trailing characters are not present. Because of this,
+    # the regular expression can fail if the pattern is at the very beginning or end of the line. To handle this case,
+    # we prefix and postfix the line with temporary characters, which we'll strip back off later.
+
+    line = "X" + line + "X"
+
+    for linkPattern in linkPatterns:
+        while True:
+            match = linkPattern.search(line)
+            if match == None: break
+            linkEndPosition = match.end() - 1
+            line = line[:linkEndPosition] + '[]' + line[linkEndPosition:]
+
+    line = line[1:-1]   # Strip off the temporary extension characters.
+
+    # In Markdeep, if you have text in square brackets not followed by parentheses or square brackets, then it's not
+    # considered to be a link, therefore you should not escape it.
+
+    while True:
+        match = literalBracketPattern.search(line)
+        if match == None: break
+        line = line[:match.start()] + '[' + line[match.start()+2 : match.end()-2] + ']' + line[match.end():]
+
     print (line)
 
 
